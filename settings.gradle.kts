9f190f4107a6dbcc2f0c78396ec23fff0df321e8
Dueris MC <purplewolf.mc1@gmail.com>
2023-11-30 17:39:48 +0000

    --skip

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,7 +22,7 @@ if (!file(".git").exists()) {
          zip from GitHub.
          
          Built Canvas jars are available for download at
-         https://canvas-project.github.io/downloads
+         https://canvas.kesug.com/downloads/
          
          See https://github.com/CraftCanvasMC/Canvas/blob/HEAD/CONTRIBUTING.md
          for further information on building and modifying Canvas.
