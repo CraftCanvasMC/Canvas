ccba57e2358906a13d87e56bf828c694a773029b
Dueris <duerismc@gmail.com>
2026-02-27 22:48:47 -0800

    Improve pearl storage logic, and config to fix pearl duplication

--- canvas-server/src/main/java/io/canvasmc/canvas/entity/EnderPearls.java
+++ canvas-server/src/main/java/io/canvasmc/canvas/entity/EnderPearls.java
@@ -96,7 +96,7 @@ public record EnderPearls(Map<UUID, List<Pearl>> pearls) {
 
     public void addPearl(final UUID uuid, final @NonNull ThrownEnderpearl thrownEnderpearl) {
         if (thrownEnderpearl.isRemoved()) {
-            Config.LOGGER.warn("Trying to add removed ender pearl, skipping");
+            Config.LOGGER.warn("Trying to add removed ({}) ender pearl, skipping", thrownEnderpearl.getRemovalReason(), new Throwable());
             return;
         }
         List<Pearl> pearls = pearls().computeIfAbsent(uuid, (ignored) -> new CopyOnWriteArrayList<>());
