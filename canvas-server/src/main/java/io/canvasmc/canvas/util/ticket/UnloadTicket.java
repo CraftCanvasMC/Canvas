b0cd5ccf12183fc5c3a16b8edafaf8e659125c6f
Dueris <duerismc@gmail.com>
2026-03-22 15:37:48 -0700

    Fixup world loading and unloading API

--- canvas-server/src/main/java/io/canvasmc/canvas/util/ticket/UnloadTicket.java
+++ canvas-server/src/main/java/io/canvasmc/canvas/util/ticket/UnloadTicket.java
@@ -1,6 +1,7 @@
 package io.canvasmc.canvas.util.ticket;
 
+import io.canvasmc.canvas.WorldUnloadResult;
 import java.util.function.Consumer;
 
-public record UnloadTicket(Consumer<Boolean> callback, boolean save) {
+public record UnloadTicket(Consumer<WorldUnloadResult> callback, boolean save) {
 }
