bb670551f2262465ae4c7e7ac6a34f77c0ad3443
MioArchive <emilioramirezziegler@gmx.de>
2026-03-28 21:26:14 +0100

    fixed logic to check simulationDistance instead of viewDistance

--- canvas-server/src/main/java/io/canvasmc/canvas/chunk/PerWorldDistanceConfig.java
+++ canvas-server/src/main/java/io/canvasmc/canvas/chunk/PerWorldDistanceConfig.java
@@ -40,6 +40,6 @@ public record PerWorldDistanceConfig(AtomicInteger viewDistance, AtomicInteger s
     }
 
     public boolean isSimulationDistanceOverridden() {
-        return this.viewDistance.get() > 0;
+        return this.simulationDistance.get() > 0;
     }
 }
