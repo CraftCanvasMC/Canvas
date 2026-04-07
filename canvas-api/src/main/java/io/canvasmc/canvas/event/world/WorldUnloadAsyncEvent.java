f6f0bb6b72afd3d79838c45172f4faea25191fe7
Dueris <122416109+Dueris@users.noreply.github.com>
2026-04-06 20:34:37 -0700

    Replace respawn event and unload event (#186)

---
+++ canvas-api/src/main/java/io/canvasmc/canvas/event/world/WorldUnloadAsyncEvent.java
@@ -0,0 +1,42 @@
+package io.canvasmc.canvas.event.world;
+
+import org.bukkit.World;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.world.WorldEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Called when a world is being unloaded via
+ * {@link org.bukkit.Server#unloadWorldAsync(org.bukkit.World, boolean, java.util.function.Consumer)}
+ *
+ * @apiNote This is always called on the <b>global region</b>
+ */
+public class WorldUnloadAsyncEvent extends WorldEvent implements Cancellable {
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    private boolean cancelled = false;
+
+    public WorldUnloadAsyncEvent(final @NotNull World world) {
+        super(world);
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(final boolean cancel) {
+        cancelled = cancel;
+    }
+}
