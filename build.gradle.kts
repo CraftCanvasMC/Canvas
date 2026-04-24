89bebb233b260568ee42e202c916746341bb76c1
Toffikk <slodkitofik@gmail.com>
2026-04-24 02:25:51 +0200

    chore: update weaver to v2.4.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import io.papermc.paperweight.core.tasks.patchroulette.AbstractPatchRouletteTask
 
 plugins {
     java
-    id("io.canvasmc.weaver.patcher") version "2.4.0"
+    id("io.canvasmc.weaver.patcher") version "2.4.1-SNAPSHOT"
     id("xyz.jpenilla.resource-factory-paper-convention") version "1.3.1" apply false
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
         gradlePluginPortal()
         maven {
             name = "canvasmc"
-            url = uri("https://maven.canvasmc.io/releases")
+            url = uri("https://maven.canvasmc.io/snapshots") // TODO: change back to releases after weaver release
         }
     }
 }
