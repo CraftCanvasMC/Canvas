861a1a855ea9a60b7548cbe0b882b714453455f4
Dueris <duerismc@gmail.com>
2026-04-27 17:40:53 -0700

    Make NumberRange validator more abstract

--- canvas-server/src/main/java/io/canvasmc/canvas/configuration/markers/NumberRange.java
+++ canvas-server/src/main/java/io/canvasmc/canvas/configuration/markers/NumberRange.java
@@ -14,9 +14,9 @@ import org.jspecify.annotations.NonNull;
 public @interface NumberRange {
     Type value();
 
-    int from() default Integer.MIN_VALUE;
+    float from() default Float.MIN_VALUE;
 
-    int to() default Integer.MAX_VALUE;
+    float to() default Float.MAX_VALUE;
 
     enum Type {
         GREATER_THAN_0,
@@ -26,32 +26,33 @@ public @interface NumberRange {
         SPECIFIC_NUMBER
     }
 
-    class Validator implements FieldValidator<NumberRange, Integer> {
+    // note that we use floats internally here, but any number type can be realistically passed into the validator
+    class Validator implements FieldValidator<NumberRange, Number> {
         @Override
-        public void validate(final String fieldName, final Integer value, final @NonNull NumberRange annotation) {
+        public void validate(final String fieldName, final Number value, final @NonNull NumberRange annotation) {
             switch (annotation.value()) {
                 case GREATER_THAN_0 -> {
-                    if (value <= 0) throw new ConfigurationValidationException(
+                    if (value.floatValue() <= 0) throw new ConfigurationValidationException(
                         fieldName, value, "must be greater than 0"
                     );
                 }
                 case LESS_THAN_0 -> {
-                    if (value >= 0) throw new ConfigurationValidationException(
+                    if (value.floatValue() >= 0) throw new ConfigurationValidationException(
                         fieldName, value, "must be less than 0"
                     );
                 }
                 case GREATER_THAN_OR_EQUAL_TO_0 -> {
-                    if (value < 0) throw new ConfigurationValidationException(
+                    if (value.floatValue() < 0) throw new ConfigurationValidationException(
                         fieldName, value, "must be greater than or equal to 0"
                     );
                 }
                 case LESS_THAN_OR_EQUAL_TO_0 -> {
-                    if (value > 0) throw new ConfigurationValidationException(
+                    if (value.floatValue() > 0) throw new ConfigurationValidationException(
                         fieldName, value, "must be less than or equal to 0"
                     );
                 }
                 case SPECIFIC_NUMBER -> {
-                    if (value < annotation.from() || value > annotation.to())
+                    if (value.floatValue() < annotation.from() || value.floatValue() > annotation.to())
                         throw new ConfigurationValidationException(
                             fieldName, value, "must be between " + annotation.from() + " and " + annotation.to()
                         );
