e148c1dcac1b57522ba04918ca2368161ed0d478
Dueris <duerismc@gmail.com>
2026-04-30 17:35:29 -0700

    Port chunk system options to new configuration system

--- canvas-server/src/main/java/io/canvasmc/canvas/GlobalConfiguration.java
+++ canvas-server/src/main/java/io/canvasmc/canvas/GlobalConfiguration.java
@@ -281,4 +281,76 @@ public class GlobalConfiguration extends Part {
         public long overloadedLogMillis = 5_000L;
         public float defaultTickRate = 20.0F;
     }
+
+    public ChunkSystem chunkSystem = new ChunkSystem();
+    public static class ChunkSystem extends Part {
+
+        {
+            option("threadPriority").between(Thread.MIN_PRIORITY, Thread.MAX_PRIORITY);
+            option("fluidPostProcessingAlgorithm")
+                .docs(
+                    Style.wrap(
+                        "The worldgen processes creates a lot of unnecessary fluid post-processing tasks,",
+                        "which can overload the server and cause stuttering when generating new chunks.",
+                        "Depending on the algorithm chosen, this can help reduce stutter and improve performance",
+                        "when generating chunks"
+                    ).defineEnum(FluidPostProcessingMode.class, (mode) -> {
+                         return switch (mode) {
+                             case VANILLA -> "Normal post processing algorithm, everything is processed";
+                             case DISABLED -> "Disables fluid post processing entirely";
+                             case FILTERED -> "C2MEs algorithm to filter unnecessary post processing tasks";
+                         };
+                    })
+                );
+
+            option("makeFluidPostProcessScheduledTick")
+                .docs(
+                    "Enabling this turns fluid post processing into a scheduled tick, which hopefully",
+                    "helps to mitigate MSPT spiking issues during chunk generation"
+                );
+            option("endBiomeCacheSize").greaterThan(0);
+            option("structureOptimizations").docs(
+                "These options are ported from the mod StructureLayoutOptimizer, https://modrinth.com/mod/structure-layout-optimizer",
+                "which optimizes the generation of Jigsaw Structures and NBT pieces"
+            );
+        }
+
+        public int threadPriority = Thread.NORM_PRIORITY;
+        public FluidPostProcessingMode fluidPostProcessingAlgorithm = FluidPostProcessingMode.VANILLA;
+
+        public enum FluidPostProcessingMode {
+            VANILLA,
+            DISABLED,
+            FILTERED;
+        }
+
+        public boolean makeFluidPostProcessScheduledTick = false;
+        public boolean optimizeAquifer = false;
+        public boolean useEndBiomeCache = false;
+        public int endBiomeCacheSize = 1024;
+        public boolean optimizeBeardifier = false;
+        public boolean optimizeNoiseGeneration = false;
+
+        public StructureGen structureOptimizations = new StructureGen();
+        public static class StructureGen extends Part {
+
+            {
+                option("deduplicateShuffledTemplatePoolElementList").docs(
+                    Style.wrap(
+                        "Whether to use an alternative strategy to make structure layouts generate slightly faster than",
+                        "the default optimization has for template pool weights. This alternative strategy works by",
+                        "changing the list of pieces that structures collect from the template pool to not have duplicate entries."
+                    )
+                    .blank()
+                    .wordWrap(
+                        "By enabling this option you can get a bit more performance from high weight Template Pool Structures,",
+                        "but you lose parity with Vanilla seeds on the layout of the structure"
+                    )
+                );
+            }
+
+            public boolean deduplicateShuffledTemplatePoolElementList = false;
+            public boolean enable = false;
+        }
+    }
 }
