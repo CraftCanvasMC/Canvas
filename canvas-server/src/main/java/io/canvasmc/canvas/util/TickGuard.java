f26340110d797ec72907887281a0f248dbd22105
Dueris <duerismc@gmail.com>
2026-05-05 19:56:39 -0700

    A bit more guards/checks

--- canvas-server/src/main/java/io/canvasmc/canvas/util/TickGuard.java
+++ canvas-server/src/main/java/io/canvasmc/canvas/util/TickGuard.java
@@ -5,26 +5,31 @@ import ca.spottedleaf.moonrise.common.util.TickThread;
 import ca.spottedleaf.moonrise.common.util.WorldUtil;
 import io.canvasmc.canvas.GlobalConfiguration;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.Level;
+import org.jspecify.annotations.NonNull;
 
 import static ca.spottedleaf.moonrise.common.util.TickThread.getThreadContext;
 import static io.canvasmc.canvas.GlobalConfiguration.LOGGER;
 
 public class TickGuard {
 
-    public static void guard(final BlockPos pos, final Level world, String reason) {
+    public static void guard(final @NonNull BlockPos pos, final Level world, String reason) {
+        guard(pos.getX() >> 4, pos.getZ() >> 4, world, reason);
+    }
+
+    public static void guard(final int chunkX, final int chunkZ, final Level world, String reason) {
         switch (GlobalConfiguration.getInstance().regionScheduler.guardSeverity) {
             case SILENT -> ensureIsTickThread(reason);
             case LOG -> {
                 // ensure tick thread first, since that is required, then we check and log
                 ensureIsTickThread(reason);
-                if (!TickThread.isTickThreadFor(world, pos)) {
-                    LOGGER.warn("Thread failed main thread check: {}, context={}, world={}, block_pos={}", reason, getThreadContext(), WorldUtil.getWorldName(world), pos, new Throwable());
+                if (!TickThread.isTickThreadFor(world, chunkX, chunkZ)) {
+                    LOGGER.warn("Thread failed main thread check: {}, context={}, world={}, chunk_pos={}", reason, getThreadContext(), WorldUtil.getWorldName(world), new ChunkPos(chunkX, chunkZ), new Throwable());
                 }
             }
-            case THROW -> TickThread.ensureTickThread(world, pos, reason);
+            case THROW -> TickThread.ensureTickThread(world, chunkX, chunkZ, reason);
         }
     }
 
