dbf57811e8d5cc0cb8c3241aaae93746bf361b7f
Dueris <duerismc@gmail.com>
2026-05-18 00:09:27 -0700

    Make ReadOnlyScore an immutable snapshot

--- canvas-server/src/main/java/io/canvasmc/canvas/world/scores/ReadOnlyScore.java
+++ canvas-server/src/main/java/io/canvasmc/canvas/world/scores/ReadOnlyScore.java
@@ -3,27 +3,32 @@ package io.canvasmc.canvas.world.scores;
 import net.minecraft.network.chat.numbers.NumberFormat;
 import net.minecraft.world.scores.ReadOnlyScoreInfo;
 import net.minecraft.world.scores.Score;
+import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 
 public final class ReadOnlyScore implements ReadOnlyScoreInfo {
-    private final Score score;
+    private final int value;
+    private final boolean locked;
+    private final NumberFormat numberFormat;
 
-    public ReadOnlyScore(Score score) {
-        this.score = score;
+    public ReadOnlyScore(final @NonNull Score score) {
+        this.value = score.value();
+        this.locked = score.isLocked();
+        this.numberFormat = score.numberFormat();
     }
 
     @Override
     public int value() {
-        return score.value();
+        return value;
     }
 
     @Override
     public boolean isLocked() {
-        return score.isLocked();
+        return locked;
     }
 
     @Override
     public @Nullable NumberFormat numberFormat() {
-        return score.numberFormat();
+        return numberFormat;
     }
 }
